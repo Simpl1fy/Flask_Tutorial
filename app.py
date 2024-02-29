ccecc5abf4a242bcadc3c38607d0d3cf8699f2a1
Gourab <gourabgaming111@gmail.com>
2024-02-29 13:20:07 +0530

    Added html to flask web application

--- app.py
+++ app.py
@@ -1,28 +1,51 @@
 # Creating url dyanmically
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello"
+def form():
+    return render_template('index.html')
 
-@app.route('/pass/<int:score>')
+    
+
+@app.route('/success/<int:score>')
 def success(score):
-    return "Person has passed with score :" + str(score)
-
-@app.route('/fail/<int:score>')
-def fail(score):
-    return "Person has failed with score: " + str(score)
-
-@app.route('/result/<int:marks>')
-def results(marks):
-    result=""
-    if marks > 50:
-        result = "success"
+    res=""
+    if score > 50:
+        res = "PASS"
     else:
-        result = "fail"
-    return redirect(url_for(result, score= marks))
+        res = "FAIL"
+    exp = {'score': score, 'res': res}
+    return render_template('results.html', result= exp)
+
+
+
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    total_score = 0
+    if request.method == 'POST':
+        math = float(request.form['math'])
+        data_science = float(request.form['data-science'])
+        c = float(request.form['c'])
+    total_score = (math + c + data_science) / 3
+    return redirect(url_for('success', score= total_score))
+
+
+
+
+# @app.route('/fail/<int:score>')
+# def fail(score):
+#     return "Person has failed with score: " + str(score)
+
+# @app.route('/result/<int:marks>')
+# def results(marks):
+#     result=""
+#     if marks > 50:
+#         result = "success"
+#     else:
+#         result = "fail"
+#     return redirect(url_for(result, score= marks))
 
 
 if __name__ == '__main__':
