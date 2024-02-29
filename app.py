44c79753151d035439feffd6867739e45e4fc9d1
Gourab <gourabgaming111@gmail.com>
2024-02-29 21:25:53 +0530

    removing comments

--- app.py
+++ app.py
@@ -33,20 +33,5 @@ def submit():
 
 
 
-
-# @app.route('/fail/<int:score>')
-# def fail(score):
-#     return "Person has failed with score: " + str(score)
-
-# @app.route('/result/<int:marks>')
-# def results(marks):
-#     result=""
-#     if marks > 50:
-#         result = "success"
-#     else:
-#         result = "fail"
-#     return redirect(url_for(result, score= marks))
-
-
 if __name__ == '__main__':
     app.run(debug=True)
